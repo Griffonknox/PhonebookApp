d1ffb27af2bc4b9ad9448566ead850094700f01c
griffonknox <thompsonscott1180@gmail.com>
2022-09-10 13:26:04 -0500

    PhonebookList edit

--- src/app/phone-book/phone-book-list/phone-book-list.component.ts
+++ src/app/phone-book/phone-book-list/phone-book-list.component.ts
@@ -6,8 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./phone-book-list.component.css']
 })
 export class PhoneBookListComponent implements OnInit {
-
-  constructor() { }
+  
+  addressBook = [
+    {firstName: "Test1",
+    lastName: "test2",
+    address: "asdflas;dkfj TX",
+    details: "friend I met at D&D"},
+    {firstName: "Test1",
+    lastName: "test2",
+    address: "asdflas;dkfj TX",
+    details: "friend I met at D&D"},
+    {firstName: "Test1",
+    lastName: "test2",
+    address: "asdflas;dkfj TX",
+    details: "friend I met at D&D"},
+]
+  
+constructor() { }
 
   ngOnInit(): void {
   }
--- src/app/phone-book/phone-book.service.ts
+++ src/app/phone-book/phone-book.service.ts
@@ -1,5 +1,18 @@
 
 
 export class PhoneBookService {
-
+    addressBook = [
+        {firstName: "Test1",
+        lastName: "test2",
+        address: "asdflas;dkfj TX",
+        details: "friend I met at D&D"},
+        {firstName: "Test1",
+        lastName: "test2",
+        address: "asdflas;dkfj TX",
+        details: "friend I met at D&D"},
+        {firstName: "Test1",
+        lastName: "test2",
+        address: "asdflas;dkfj TX",
+        details: "friend I met at D&D"},
+    ]
 }
