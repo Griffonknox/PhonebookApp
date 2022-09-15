38b9a817367ab35807e75ace863fbc9fa6fbbf46
Scott Thompson <thompsonscott11180@gmail.com>
2022-09-15 12:38:25 -0500

    finished directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,37 +1,34 @@
 import { Directive, HostListener, HostBinding, ElementRef, Renderer2 } from '@angular/core';
+import { PhoneBookService } from '../phone-book/phone-book.service';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {
+  constructor(private el: ElementRef, private renderer: Renderer2,
+    private phoneBookService: PhoneBookService) {
     this.renderer.addClass(this.el.nativeElement, "open");
   }
 
-  @HostBinding('class.open') isOpen = true;
+    @HostBinding('class.open') isOpen = false;
+    @HostListener('document:click', ['$event']) toggleOpen2(event: Event) {
 
-  @HostListener('click') toggleOpen() {
-    console.log("testing directive fire");
-    let pBody = this.el.nativeElement.querySelector('.panel-body');
-    if (this.isOpen) {
-      this.renderer.removeClass(pBody, "collapse");
-      this.isOpen = false;
-    } else {
-      this.renderer.addClass(pBody, "collapse");
-      this.isOpen = true;
+      let pHead = this.el.nativeElement.querySelector('.panel-heading')
+      let pBody = this.el.nativeElement.querySelector('.panel-body');
+      this.isOpen = this.el.nativeElement.contains(event.target) ? !this.isOpen : false;
+      if (this.isOpen) {
+        this.renderer.removeClass(pBody, "collapse");
+        this.renderer.addClass(pHead, "select-color");
+      } else {
+        this.renderer.addClass(pBody, "collapse");
+        this.renderer.removeClass(pHead, "select-color");
+      }
+
+      
     }
-  }
 
 
-  // @HostListener('document:click', ['$event']) toggleClose(event: Event) {
-  //   console.log("testing directive fire");
-  //   let pBody = this.el.nativeElement.querySelector('.panel-body');
-  //   if (!this.isOpen) {
-  //     this.renderer.addClass(pBody, "collapse");
-  //     this.isOpen = true;
-  //   }
-  // }
 
 
 
