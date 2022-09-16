d4365365850646bb9fb612c0133b59e6e1cf8a88
griffonknox <thompsonscott1180@gmail.com>
2022-09-15 19:00:31 -0500

    polished the popupscreen

--- src/app/phone-book/phone-book.service.ts
+++ src/app/phone-book/phone-book.service.ts
@@ -23,7 +23,10 @@ export class PhoneBookService {
     phBook: {[id: number]: PhoneBook} = {
         1: new PhoneBook("Scott", "Thompson", "956-451-6159", "1749 E 14th Pl", "Tulsa", "Oklahoma", "74104", "Owner and Builder of App."),
         2: new PhoneBook("Bill", "Stevens", "314-945-8796", "3301 Armour St.", "St. Louis", "Missouri", "63028", "Met at D@D."),
-        3: new PhoneBook("Jessica", "Meyers", "180-466-9987", "773 Detroit Blvd.", "McAllen", "Texas", "84665", "Taekwondo specialist from Trivia.")
+        3: new PhoneBook("Jessica", "Meyers", "180-466-9987", "773 Detroit Blvd.", "McAllen", "Texas", "84665", "Taekwondo specialist from Trivia."),
+        4: new PhoneBook("Bethany", "Demitri", "816-468-7945", "1928 Harvard", "Tulsa", "Oklahoma", "74104", "I guess this person is someone that I need to think of is a good reason to add to this app?"),
+        5: new PhoneBook("Sebastion", "McIntosh", "664-789-1664", "3912 Ickoris Dr.", "Kareoki", "Wyoming", "79468", "Astronomy Convention in Hawaii; wore a dop ass tie."),
+        6: new PhoneBook("Carl", "Jeffries", "314-468-4687", "1172 Grok St.", "Festus", "Missouri", "63114", "Lost Ark, the Scrapper that was rank 1400 something.")
     }
 
     filter(obj: any) {
